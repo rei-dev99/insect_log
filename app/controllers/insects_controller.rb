5626e4e909550a422286842c0640285af44f532f
rei <idareiya123@gmail.com>
2024-04-29 13:10:40 +0900

    ok

--- app/controllers/insects_controller.rb
+++ app/controllers/insects_controller.rb
@@ -3,9 +3,6 @@ class InsectsController < ApplicationController
   end
 
   def show
-    # place = params[:place]
-    # @insect = Insect.where(place: place).order("RANDOM()").first
-    # @insect = INSECTS.sample # 配列からランダムに一つ選ぶ
     place = params[:place]
     @insects = INSECTS.select { |insect| insect[:place] == place }
     @insect = @insects.sample
